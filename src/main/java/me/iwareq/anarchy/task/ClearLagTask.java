cd4ea99b364f257da24e193129c3c59d41d2556c
IWareQ <dimonpo96@mail.ru>
2022-10-13 11:53:18 +0400

    feat(task): add auto clear lag task

---
+++ src/main/java/me/iwareq/anarchy/task/ClearLagTask.java
@@ -0,0 +1,48 @@
+package me.iwareq.anarchy.task;
+
+import cn.nukkit.Player;
+import cn.nukkit.Server;
+import cn.nukkit.entity.Entity;
+import cn.nukkit.scheduler.Task;
+import me.iwareq.anarchy.AnarchyCore;
+
+public class ClearLagTask extends Task {
+
+	private static final int CLEAR_DELAY = 20 * 60;
+
+	private int left = CLEAR_DELAY;
+
+	@Override
+	public void onRun(int currentTick) {
+		this.left--;
+
+		if (this.left == 5 * 60) {
+			Server.getInstance().broadcastMessage(AnarchyCore.PREFIX + "Очистка произойдет через 5 минут!");
+		}
+
+		if (this.left <= 0) {
+			this.clearAll();
+
+			for (Player player : Server.getInstance().getOnlinePlayers().values()) {
+				player.sendMessage("§l§7(§3Очистка§7) §rОчистка завершена§7!");
+				player.sendTip("Очистка завершена!");
+			}
+
+			this.left = CLEAR_DELAY;
+		}
+	}
+
+	private void clearAll() {
+		Server.getInstance().getLevels().values().forEach(level -> {
+			for (Entity entity : level.getEntities()) {
+				if (!(entity instanceof Player)) {
+					entity.close();
+				}
+			}
+
+			level.doChunkGarbageCollection();
+		});
+
+		System.gc();
+	}
+}
