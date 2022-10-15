546ef8a46b31ff225e1c477b5be1987727828afa
IWareQ <dimonpo96@mail.ru>
2022-10-15 17:18:04 +0400

    chore(sql): change money format

--- sql/players.sql
+++ sql/players.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS Players
 (
     ID       INTEGER PRIMARY KEY AUTOINCREMENT,
     Username VARCHAR(32) NOT NULL COLLATE NOCASE,
-    Money    INT         NOT NULL DEFAULT '0.0'
+    Money    VARCHAR(32) NOT NULL DEFAULT '0.0'
 );
 
 DROP TABLE Players;
@@ -10,6 +10,16 @@ DROP TABLE Players;
 INSERT INTO Players (Username)
 VALUES (:username);
 
+INSERT INTO Players (Username)
+VALUES ('IWareQQ');
+
 UPDATE Players
 SET Money = :money
 WHERE Username = :username;
+
+UPDATE Players
+SET Money = '1.0'
+WHERE Username = 'iwareqq';
+
+SELECT *
+FROM Players;
